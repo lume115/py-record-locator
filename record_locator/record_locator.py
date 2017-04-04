6cd942274c4b4d1771875a5445566ebaab1a7957
Lukasz Budryk <lb@alladin.at>
2017-04-04 13:46:11 +0200

    initial commit

---
+++ record_locator/record_locator.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+DEFAULT_BASE_CHARACTERS = "23456789ACDEFGHJKLMNPRTUVWXYZ"
+
+
+class RecordLocator:
+    _base_characters = DEFAULT_BASE_CHARACTERS
+    _mapped_integers = None
+
+    def __init__(self, base_characters=None):
+        if base_characters is not None:
+            self._base_characters = base_characters
+
+        self._mapped_integers = {v: k for k, v in enumerate(self._base_characters)}
+
+    def encode(self, id_num):
+        ints = []
+        while id_num != 0:
+            ints.insert(0, id_num % len(self._base_characters))
+            id_num //= len(self._base_characters)
+        rec = ""
+        for i in ints:
+            rec += self._base_characters[i]
+
+        return rec
+
+    def decode(self, rec):
+        id_num = 0
+        for i in rec.upper():
+            id_num = (id_num * len(self._base_characters)) + self._mapped_integers[i]
+
+        return id_num
---
+++ record_locator/test_record_locator.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+
+from record_locator.record_locator import RecordLocator
+
+
+class TestRecordLocator(unittest.TestCase):
+
+    def test_default_record_locator(self):
+        rl = RecordLocator()
+        self.assertEqual("5G", rl.encode(100))
+        self.assertEqual(100, rl.decode("5G"))
+        self.assertEqual("ZNYE7", rl.encode(20290290))
+        self.assertTrue(rl.encode(20290290).isupper())
+        self.assertEqual(20290290, rl.decode("ZNYE7"))
+        self.assertEqual(20290290, rl.decode(rl.encode(20290290)))
+
+    def test_custom_record_locator(self):
+        rl = RecordLocator(base_characters="ABCDEFGHIJKL")
+        self.assertEqual("IE", rl.encode(100))
+        self.assertEqual(100, rl.decode("IE"))
+        self.assertEqual("GJGGAJG", rl.encode(20290290))
+        self.assertTrue(rl.encode(20290290).isupper())
+        self.assertEqual(20290290, rl.decode("GJGGAJG"))
+        self.assertEqual(20290290, rl.decode(rl.encode(20290290)))
