f46026a284ad9f3ebc43d78fca0a8c2e7a302507
Lukasz Budryk <lb@alladin.at>
2017-04-04 16:06:21 +0200

    add checksum generation and update README

--- record_locator/record_locator.py
+++ record_locator/record_locator.py
@@ -7,30 +7,58 @@ from __future__ import print_function
 DEFAULT_BASE_CHARACTERS = "234679ACDEFGHJKLMNPRTUVWXYZ"
 
 
+class ChecksumException(Exception):
+    pass
+
+
 class RecordLocator:
     _base_characters = DEFAULT_BASE_CHARACTERS
     _mapped_integers = None
+    _has_check_digit = False
+    _prime = 0
 
-    def __init__(self, base_characters=None):
+    def __init__(self, base_characters=None, has_check_digit=False):
         if base_characters is not None:
             self._base_characters = base_characters
 
+        self._has_check_digit = has_check_digit
         self._mapped_integers = {v: k for k, v in enumerate(self._base_characters)}
+        self._prime = len(self._base_characters) + 1
 
     def encode(self, id_num):
         ints = []
-        while id_num != 0:
-            ints.insert(0, id_num % len(self._base_characters))
-            id_num //= len(self._base_characters)
+        id_num_mod = id_num
+        while id_num_mod != 0:
+            ints.insert(0, id_num_mod % len(self._base_characters))
+            id_num_mod //= len(self._base_characters)
         rec = ""
         for i in ints:
             rec += self._base_characters[i]
+
+        if self._has_check_digit:
+            rec += self._generate_check_digit(ints)
 
         return rec
 
     def decode(self, rec):
         id_num = 0
-        for i in rec.upper():
+        end = -1 if self._has_check_digit else None
+        for i in rec.upper()[:end]:
             id_num = (id_num * len(self._base_characters)) + self._mapped_integers[i]
 
+        if self._has_check_digit:
+            if not self._is_valid_checksum(rec):
+                raise ChecksumException("Incorrect checksum for {}".format(rec))
+
         return id_num
+
+    def _generate_check_digit(self, ints):
+        checksum = 0
+        for i, v in enumerate(ints):
+            checksum+=v*(i+1)
+
+        return self._base_characters[checksum % len(self._base_characters)]
+
+    def _is_valid_checksum(self, rec):
+        ints = [self._mapped_integers[r] for r in rec[:-1]]
+        return self._generate_check_digit(ints) == rec[-1:]
--- record_locator/test_record_locator.py
+++ record_locator/test_record_locator.py
@@ -28,3 +28,12 @@ class TestRecordLocator(unittest.TestCase):
         self.assertTrue(rl.encode(20290290).isupper())
         self.assertEqual(20290290, rl.decode("GJGGAJG"))
         self.assertEqual(20290290, rl.decode(rl.encode(20290290)))
+
+    def test_check_digit(self):
+        rl = RecordLocator(has_check_digit=True)
+        self.assertEqual("6RK", rl.encode(100))
+        self.assertEqual(100, rl.decode("6RK"))
+        self.assertEqual("3G7W3AA", rl.encode(20290290))
+        self.assertTrue(rl.encode(20290290).isupper())
+        self.assertEqual(20290290, rl.decode("3G7W3AA"))
+        self.assertEqual(20290290, rl.decode(rl.encode(20290290)))
